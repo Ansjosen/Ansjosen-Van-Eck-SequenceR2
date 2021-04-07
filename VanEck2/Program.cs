aea221d01bec9ff83c2be23724e1800d4e2e9ca8
PushRod <barfred.dk@gmail.com>
2021-04-07 15:35:36 +0200

    Grouping counts by start values 0 - 99

--- VanEck2/Program.cs
+++ VanEck2/Program.cs
@@ -10,71 +10,86 @@ namespace VanEck2
     {
         static void Main(string[] args)
         {
+            //next challenge: longest pending number in each serie, 00 01 02 03 ... 99
             List<int> list = new List<int>();
             int elementAtLastIndex = -1;
             int elementLast = -1;
-            list.Add(50); list.Add(0);
+            int max = 0, min = 1000000, minValue =-1, maxValue =-1;
+            //int[] arr;
 
-            List<string> dinosaurs = new List<string>();
-
-            dinosaurs.Add("Tyrannosaurus");     //0
-            dinosaurs.Add("Amargasaurus");      //1
-            dinosaurs.Add("Mamenchisaurus");    //2
-            dinosaurs.Add("Brachiosaurus");     //3
-            dinosaurs.Add("Deinonychus");       //4
-            dinosaurs.Add("Tyrannosaurus");     //5
-            dinosaurs.Add("Tyrannosaurus");     //6
-            //count = 7 (dinosaurer)
-            int dino = dinosaurs.LastIndexOf("Tyrannosaurus", dinosaurs.Count - 2); // == 5
-                dino = dinosaurs.LastIndexOf("Tyrannosaurus", dinosaurs.Count - 1); // == 6
-                dino = dinosaurs.LastIndexOf("Tyrannosaurus", 3);                   // == 0
-
-            //Best of 10 --> 90
-
-            //int nrOfLoops = 1882, countLoops = 0;  // --> 3,0 VINDER + loops nødvendig for alle tal fra 0 - 100
-            //int nrOfLoops = 3856, countLoops = 0;  // --> 4,0 VINDER + loops nødvendig for alle tal fra 0 - 100
-
-            //provet fra 10,0 - 30,0
-            //int nrOfLoops = 1596, countLoops = 0;  // --> 17,0 VINDER + loops nødvendig for alle tal fra 0 - 100
-            //int nrOfLoops = 6978, countLoops = 0;  // --> 12,0 VINDER + loops nødvendig for alle tal fra 0 - 100
-            int nrOfLoops = 1596, countLoops = 0;  // --> 12,0 VINDER + loops nødvendig for alle tal fra 0 - 100
-            //29 27 22 21 12
-            //001020221605...
-            do
+            for (int i = 0; i < 101; i++)
             {
-                countLoops++;
-                elementLast = -1;
-                elementAtLastIndex = -1;
+                list.Clear();
+                list.Add(i); list.Add(0);
+
+                int nrOfLoops = 10000, countLoops = 0;  // --> 17,0 VINDER + loops nødvendig for alle tal fra 0 - 100
+                
+                do
+                {
+                    countLoops++;
+                    elementLast = -1;
+                    elementAtLastIndex = -1;
                                 
-                elementLast = list.ElementAt(list.Count - 1);
-                elementAtLastIndex = list.LastIndexOf(elementLast, list.Count - 2);
-                if (elementAtLastIndex == -1)
+                    elementLast = list.ElementAt(list.Count - 1);
+                    elementAtLastIndex = list.LastIndexOf(elementLast, list.Count - 2);
+                    if (elementAtLastIndex == -1)
+                    {
+                        list.Add(0);
+                    }
+                    else
+                    {
+                        elementAtLastIndex = list.Count - 1 - elementAtLastIndex;
+                        //Den fundne position trækkes fra listens længde for at finde afstanden bagud
+                        list.Add(elementAtLastIndex);
+                    }
+
+                    var arr = list.ToArray().Distinct().ToArray();
+                    Array.Sort(arr);
+
+
+                    if (arr.Length >= 100 )
+                    {
+                        if (IsOk(arr))
+                        {
+                            //Console.WriteLine("Go!");
+                            break; ;
+                        }
+                        else
+                        {
+                            //NOP
+                        }
+                    }
+                    
+
+
+
+                } while (countLoops <= nrOfLoops);
+                
+                
+                //list.Add(0); list.Add(1); list.Add(0); list.Add(2); list.Add(0); list.Add(1); list.Add(6);
+                string combindedString = string.Join(",", list.ToArray());
+                //Console.WriteLine(combindedString);
+                //Console.WriteLine("Distinct:");  // oldArray.Distinct().ToArray();
+                //arr =  list.ToArray().Distinct().ToArray();
+                //Array.Sort(arr);
+                //combindedString = string.Join(",", arr);
+                //Console.WriteLine(combindedString);
+                if (countLoops < min)
                 {
-                    list.Add(0);
+                    minValue = i;
+                    min = countLoops;
                 }
-                else
+                if (countLoops > max)
                 {
-                    elementAtLastIndex = list.Count - 1 - elementAtLastIndex;
-                    //Den fundne position trækkes fra listens længde for at finde afstanden bagud
-                    list.Add(elementAtLastIndex);
+                    max = countLoops;
+                    maxValue = i;
+
                 }
-            } while (countLoops <= nrOfLoops);
-            //list.Add(0); list.Add(1); list.Add(0); list.Add(2); list.Add(0); list.Add(1); list.Add(6);
-            string combindedString = string.Join(",", list.ToArray());
-            Console.WriteLine(combindedString);
-            Console.WriteLine("Distinct:");  // oldArray.Distinct().ToArray();
-            var arr =  list.ToArray().Distinct().ToArray();
-            Array.Sort(arr);
-            combindedString = string.Join(",", arr);
-            Console.WriteLine(combindedString);
-            if (IsOk(arr))
-            {
-                Console.WriteLine("GO!");
+
+                Console.WriteLine("Value: " + i + ", #loops " + countLoops);
             }
-            else
-            {
-                Console.WriteLine("NoGo");
-            }
+            Console.WriteLine("min: " + minValue + ": " + min);
+            Console.WriteLine("max: " + maxValue + ": " + max);
             Console.ReadLine();
         }
 
@@ -90,10 +105,11 @@ namespace VanEck2
             {
                 if (arr[i] != i)
                 {
-                    Console.WriteLine("Element at arr[" + i + "] " + arr[i] );
+                    //Console.WriteLine("Element at arr[" + i + "] " + arr[i] );
                     return false;
                 }
             }
+            
             return true;
         }
     }
